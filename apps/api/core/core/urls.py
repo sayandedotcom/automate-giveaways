638f14da44ca2fc70dff009f4c9226f3ea575359
Sayan De <sayandeten@gmail.com>
2024-06-14 13:12:41 +0530

    new

--- apps/api/core/core/urls.py
+++ apps/api/core/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend_api project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage),
+    path('about/', views.aboutpage),
 ]
